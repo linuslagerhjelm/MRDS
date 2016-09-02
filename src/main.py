b50095cc01bcee6204dfd8b19bc7850891ed60c2
syltman <mcoquand@gmail.com>
2016-09-02 15:17:42 +0200

    Modified main

--- src/main.py
+++ src/main.py
@@ -1,12 +1,13 @@
 import sys
 import mrdsapi
 
-#Input argument address, port, lookahead
+#Input argument address, port, lookahead, path to path file
 def main():
     ADDRESS = sys.argv[1]
     PORT = sys.argv[2]
     LOOKAHEAD = sys.argv[3]
     mrds = Mrdsapi(ADDRESS, PORT)
+    path_file = Path(path)
     rob = Robot(mrds)
 
 if __name__ == "__main__":
